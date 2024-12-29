11dc31e653b635103475bfd0c732e85153bb04f8
MD EZAZUL HASSAN BISWAS <mdezazulhassanbiswas@gmail.com>
2024-12-29 14:44:21 +0600

    Delete url.js

--- url.js
+++
@@ -1,5 +0,0 @@
-
-// Remove protocol and www from the URL in the address bar
-const currentUrl = window.location.href;
-const plainDomain = currentUrl.replace(/^https?:\/\/(www\.)?/, 'mdezazulhassanbiswas.gihub.io');
-history.replaceState(null, null, plainDomain);
