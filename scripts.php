4ac6581386e37dc841b61080a6ad22143f307488
MD EZAZUL HASSAN BISWAS <mdezazulhassanbiswas@gmail.com>
2025-02-13 12:29:20 +0600

    jhgj

---
+++ scripts.php
@@ -0,0 +1,244 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta name="theme-color" content="hsl(240, 100%, 50%)" id="meta-theme-color">
+
+    <meta name="author" content="Md Ezazul Hassan Biswas, mdezazulhassanbiswas@gmail.com, +8801602873384">
+    <meta name="keywords" content="
+      web developer | md ezazul hassan biswas,front-end web designer & developer,md ezazul hassan biswas,+8801602873384,mdezazulhassanbiswas@gmail.com,mdezazulhassanbiswas,@mdezazulhassanbiswas,#mdezazulhassanbiswas,
+      website developer,web designer and developer,web developer,hire web developer,redesign website,web developer website,web developer online,front-end developer,mobile ready website,website designer,
+      web developer for hire,web designer near you,web designer jessore district,your local web designer,web developer near you,web developer jessore district,your local web developer,
+      web designer,web developer,front-end developer,front-end designer,html,html expert,css master,javascript,responsive web design,ux design,ui design,user experience,user interface,mobile-first design,cross-browser compatibility,web standards,bootstrap,web customization,e-commerce solutions,web performance optimization,
+      seo-friendly design,web accessibility,modern web design,interactive web pages,clean code,web animations,css,flexbox and grid layout,javaScript libraries,ront-end frameworks,web components,progressive web apps,performance metrics,web design inspiration,landing page design,portfolio website,web design trends,github projects,
+      web development best practices,freelance web developer,creative web design,cutting-edge web technologies,front-end development tools,visual design,custom web solutions,digital marketing,online branding,professional web services,custom layouts,client-side scripting,modern web applications,interactive user interfaces,dynamic web pages,
+      search engine optimization (seo),web analytics,web development,website design,responsive design,web development trends,web performance,e-commerce,ui/ux design trends,
+      responsive design trends,user-friendly websites"
+    >
+
+    <meta name="description" content="Md Ezazul Hassan Biswas is a skilled web designer and front-end developer. Specializing in responsive and user-friendly websites, visit mdezazulhassanbiswas.github.io to learn more. Contact me at mdezazulhassanbiswas@gmail.com or +8801602873384.">
+    <meta property="og:description" content="Md Ezazul Hassan Biswas is a skilled web designer and front-end developer. Specializing in responsive and user-friendly websites, visit mdezazulhassanbiswas.github.io to learn more. Contact me at mdezazulhassanbiswas@gmail.com or +8801602873384.">
+    <meta name="twitter:description" content="Md Ezazul Hassan Biswas is a skilled web designer and front-end developer. Specializing in responsive and user-friendly websites, visit mdezazulhassanbiswas.github.io to learn more. Contact me at mdezazulhassanbiswas@gmail.com or +8801602873384.">
+
+    <meta name="robots" content="index, follow">
+    <meta property="og:url" content="https://mdezazulhassanbiswas.github.io">
+
+    <meta name="twitter:card" content="summary_large_image">
+  
+    <link rel="shortcut icon" href="/assets/images/logo-dark.png" type="image/x-icon">
+    <meta property="og:image" content="/assets/images/logo-dark.png">
+    <meta name="twitter:image" content="/assets/images/logo-dark.png">
+
+    <title>Web Designer & Front-End Developer | Md Ezazul Hassan Biswas</title>
+    <meta property="og:title" content="Web Designer & Front-End Developer | Md Ezazul Hassan Biswas">
+    <meta name="twitter:title" content="Web Designer & Front-End Developer | Md Ezazul Hassan Biswas">
+    <meta name="subtitle" content="Web Designer & Front-End Developer">
+
+    <link rel="stylesheet" href="styles.css">
+    <link rel="manifest" href="/manifest.webmanifest">
+    <script>
+        if ('serviceWorker' in navigator) {
+          navigator.serviceWorker.register('/sw.js')
+            .then(registration => {
+              console.log('Service worker registration successful:', registration);
+            })
+            .catch(error => {
+              console.log('Service worker registration failed:', error);
+            });
+        }
+    </script>
+    
+    <!-- <?php
+        function updateMetaDate() {
+            $currentDate = date("Y-m-d");
+            echo '<meta name="date" content="' . $currentDate . '" scheme="YYYY-MM-DD">';
+        }
+        updateMetaDate()
+    ?> -->
+
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-NWB3RK8474"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-NWB3RK8474');
+    </script>
+
+    <script src="https://kit.fontawesome.com/9eeb43021d.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <section id="body-notification-container">
+        <section class="top"></section>
+        <section class="middle"></section>
+        <section class="bottom"></section>
+    </section>
+
+    <section id="body-content-container">
+        <!-- data speed (20) === page load time -->
+        <section id="loader" class="grid" data-speed="15" data-duration="100" data-delay="0">
+            <section class="progress">
+                <div class="back-ground"></div>
+                <div class="fore-ground"></div>
+            </section>
+        </section>
+
+        <nav id="tab-switcher" class="container-grid"></nav> <!-- home & Short-videos -->
+
+        <header id="header" class="container-grid">
+            <nav id="nav" class="flex x-center y-center">
+                <ul class="nav-list" id="header-nav-list">
+                    <li class="list-item active-bg"><a href=""></a></li>
+                    <li class="list-item active"><a href="#hero">Home</a></li>
+                    <li class="list-item"><a href="#about-me">About me</a></li>
+                    <!-- <li class="list-item"><a href="#projects">Projects</a></li> -->
+                    <!-- <li class="list-item"><a href="#faqs">FAQs</a></li> -->
+                </ul>
+            </nav>
+        </header>
+        
+        <section id="main-wrapper"> <!-- .5em padding wight accent bg -->
+            <main class="container-grid">
+                <section id="hero">
+                    <section id="scroller">
+                        <section class="slider-container col">
+                            <div class="slide">
+                                <img class="" src="assets/images/2020-06-17.jpg" alt="slide img">
+                            </div>
+                            <div class="slide">
+                                <img class="" src="assets/images/2020-06-01.jpg" alt="slide img">
+                            </div>
+                            <div class="slide">
+                                <img class="" src="assets/images/IMG_20230622_165721.jpg" alt="slide img">
+                            </div>
+                        </section>
+                        <section class="col y-center x-center fade-in-top-container">
+                            <div class="left row x-end">
+                                <h6 class="txt-right fade-in-top">Web Designer</h6>
+                                <h2 class="txt-right fade-in-top">Design and Develop web all around the world</h2>
+                            </div>
+                            <div class="right row y-end">
+                                <p class="fade-in-top">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem officia aliquam voluptatem aut, veritatis libero cumque quas debitis alias quod, totam maxime rerum repellendus nisi.
+                                </p>
+                                <a class="fade-in-top" href="#">learn more →</a>
+                            </div>
+                        </section>
+                        <section class="col y-center x-center fade-in-top-container">
+                            <div class="left row x-end">
+                                <h6 class="txt-right fade-in-top">Web Developer</h6>
+                                <h2 class="txt-right fade-in-top">Design and Develop web all around the world</h2>
+                            </div>
+                            <div class="right row y-end">
+                                <p class="fade-in-top">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem officia aliquam voluptatem aut, veritatis libero cumque quas debitis alias quod, totam maxime rerum repellendus nisi.
+                                </p>
+                                <a class="fade-in-top" href="#">learn more →</a>
+                            </div>
+                        </section>
+                        <section class="col y-center x-center fade-in-top-container">
+                            <div class="left row x-end">
+                                <h6 class="txt-right fade-in-top">Let's work together</h6>
+                                <h2 class="txt-right fade-in-top">Design and Develop web all around the world</h2>
+                            </div>
+                            <div class="right row y-end">
+                                <p class="fade-in-top">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem officia aliquam voluptatem aut, veritatis libero cumque quas debitis alias quod, totam maxime rerum repellendus nisi.
+                                </p>
+                                <a class="fade-in-top" href="#">learn more →</a>
+                            </div>
+                        </section>
+                    </section>
+                    <section id="get-scroller-scroll-width"></section>
+                </section>
+        
+                <section id="about-me" class="row">
+                    <h2 class="text-revel-onscroll">About Me</h2>
+                    <p>
+                        <span  class="text-revel-onscroll">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo sint sed ad, officia dolorem eligendi quam culpa. Hic esse fuga reiciendis, provident voluptas tempora excepturi porro maxime ad voluptates, quasi iure sint perspiciatis minus, libero necessitatibus in dolor culpa nulla.
+                        </span>
+                    </p>
+                </section>
+        
+                <section id="projects" class="row">
+                    <h2 class="text-revel-onscroll">Most Recent Projects</h2>
+                    <p>
+                        <span  class="text-revel-onscroll">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo sint sed ad, officia dolorem eligendi quam culpa. Hic esse fuga reiciendis, provident voluptas tempora excepturi porro maxime ad voluptates, quasi iure sint perspiciatis minus, libero necessitatibus in dolor culpa nulla.
+                        </span>
+                    </p>
+                </section>
+        
+                <section id="faqs" class="content-page col surface-0">
+                    <section class="row py-x py-y">
+                        <h6>FAQs</h6>
+                        <h2>Frequently Ask Questions</h2>
+                        <p>
+                            <span class="text-revel-onscroll">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci rem magnam molestiae beatae quas laborum!
+                            </span>
+                        </p>
+                    </section>
+                    <section class="row">
+                        <ul class="row question-container py-y">
+                            <li class="question-answer">
+                                <ul class="row  py-x ">
+                                    <li class="question py-y flex x-between"> <div class="flex"> <b>01.</b> <b>question</b> </div> <span class="indicator">+</span> </li>
+                                    <li class="answer">
+                                        <p>
+                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. A porro exercitationem voluptate dolore quos veritatis?
+                                        </p>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="question-answer">
+                                <ul class="row  py-x ">
+                                    <li class="question py-y flex x-between"> <div class="flex"> <b>02.</b> <b>question</b> </div> <span class="indicator">+</span> </li>
+                                    <li class="answer">
+                                        <p>
+                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. A porro exercitationem voluptate dolore quos veritatis?
+                                        </p>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="question-answer">
+                                <ul class="row  py-x ">
+                                    <li class="question py-y flex x-between"> <div class="flex"> <b>03.</b> <b>question</b> </div> <span class="indicator">+</span> </li>
+                                    <li class="answer">
+                                        <p>
+                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. A porro exercitationem voluptate dolore quos veritatis?
+                                        </p>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </section>
+                </section>
+        
+            </main>
+        </section>
+    
+        
+    
+        <footer class="container-grid">
+            <section class="content-page flex x-between">
+                <p> &copy;<span class="year">2025</span> <img class="brand-sm icon" src="/assets/images/logo-light.png" alt="Web Designer & Front-End Developer | Ezazul Hassan Biswas"> <span class="txt-accent">MEHB</span></p>
+                <div class="flex x-center">
+                    <a class="icon" href="http://fb.com/mdezazulhassanbiswas" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-facebook"></i></a>
+                    <a class="icon" href="http://instagram.com/mdezazulhassanbiswas" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-instagram"></i></a>
+                    <a class="icon" href="http://linkedin.com/in/mdezazulhassanbiswas" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-linkedin"></i></a>
+                </div>
+            </section>
+        </footer>
+    </section>
+    
+    <script src="scripts.js"></script>
+</body>
+</html>
