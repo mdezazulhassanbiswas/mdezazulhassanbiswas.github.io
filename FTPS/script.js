2cb355f2895b66764f4a46ae44ec19a751f3a216
Web Developer | Hassan Biswas <mdezazulhassanbiswas@gmail.com>
2025-10-21 13:12:59 +0600

    Create script.js

---
+++ FTPS/script.js
@@ -0,0 +1,13 @@
+// TEMPORARY LOCAL TESTING CONFIG
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "...",
+  appId: "...",
+};
+const initialAuthToken = null; // Use null for local anonymous sign-in
+// END OF TEMPORARY CONFIG
+    (Note: You would need to temporarily hardcode these values to test outside the collaborative canvas.)
+      
