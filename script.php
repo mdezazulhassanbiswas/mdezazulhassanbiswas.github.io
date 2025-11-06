39e6cdd9c730ad283eb150a4e44605df82e60aa9
Hassan Biswas <mdezazulhassanbiswas@gmail.com>
2025-11-06 11:49:26 +0600

    h

--- script.php
+++
@@ -1,7 +0,0 @@
-<?php
-    function updateMetaDate() {
-        $currentDate = date("Y-m-d");
-        echo '<meta name="date" content="' . $currentDate . '" scheme="YYYY-MM-DD">';
-    }
-    updateMetaDate()
-?>
